eac533e6e141c011172a402cf276100cea1a34d8
alessandro.rossi <alessandro.rossi@sourcesense.com>
2020-08-19 17:58:28 +0200

    Added mongo index on training's creationDate.

--- src/main/java/com/nirshal/util/mongodb/MongoCollections.java
+++ src/main/java/com/nirshal/util/mongodb/MongoCollections.java
@@ -1,10 +1,14 @@
 package com.nirshal.util.mongodb;
 
 import com.mongodb.client.MongoCollection;
+import org.bson.conversions.Bson;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Manges all the collections defined in the project.
@@ -20,16 +24,47 @@ public class MongoCollections {
     MongoClientHelperQuarkus mongodb;
 
     /**
+     * A Map holding instances of repositories in order to avoid instanciating multiple identical collections.
+     */
+    private Map<String, MongoRepository> collections = new HashMap<>();
+
+    /**
      * Returns the specified repository.
      * @param type the specific repository we want.
      * @return
      */
     public MongoRepository getRepositoryFrom(Class type) {
 
-        logger.info("Mongo Repository intialization for type={}.", type.getSimpleName());
-        MongoCollection collection = mongodb.getCollection(type.getSimpleName(), type);
+        if (
+                collections.get(type.getSimpleName())!= null
+        ){
+            logger.info("Mongo Repository for type={} has been already instantiated: returning the existing instance.", type);
+            return collections.get(type.getSimpleName());
+        } else {
+            logger.info("Mongo Repository initialization for type={}.", type.getSimpleName());
+            MongoCollection collection = mongodb.getCollection(type.getSimpleName(), type);
 
-        return new MongoRepository(collection);
+            for (Bson index : MongoCollectionsIndexesProvider.getIdexes(type.getSimpleName())) {
+                try {
+                    String indexName = collection.createIndex(index);
+                    logger.info("Creating index on Mongo Repository type={}, index={}",
+                            type,
+                            indexName
+                    );
+                } catch (Exception e) {
+
+                    logger.error("Failed to create index={} on repository with type={}, exception={}",
+                            index,
+                            type,
+                            e
+                    );
+                }
+            }
+            MongoRepository repo = new MongoRepository(collection);
+            // We save the new repo reference for future reuse, if configured so.
+            collections.put(type.getSimpleName(), repo);
+            return repo;
+        }
     }
 
 }
--- src/main/java/com/nirshal/util/mongodb/MongoCollectionsIndexesProvider.java
+++ src/main/java/com/nirshal/util/mongodb/MongoCollectionsIndexesProvider.java
@@ -1,32 +1,33 @@
-//package com.nirshal.util.mongodb;
-//import org.bson.conversions.Bson;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//public class MongoCollectionsIndexesProvider {
-//
-//    /**
-//     * Maps the Collection type cases with the Indexes that must be present for them.
-//     * @param type
-//     * @return
-//     */
-//    public static List<Bson> getIdexes(MongoCollections.type type) {
-//
-//        List<Bson> indexes = new ArrayList<>();
-//
-//        switch (type){
-//            case TRAININGS:
-////                indexes.add(
-////                        Indexes.compoundIndex(
-////                                Indexes.ascending(FieldConstants.MONGO_ID),
-////                                Indexes.ascending(RestConstants.USER_LEVEL)
-////                        )
-////                );
-//                break;
-//        }
-//
-//        return indexes;
-//    }
-//
-//}
+package com.nirshal.util.mongodb;
+import com.mongodb.client.model.Indexes;
+import com.nirshal.model.Training;
+import org.bson.conversions.Bson;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MongoCollectionsIndexesProvider {
+
+    /**
+     * Maps the Collection type cases with the Indexes that must be present for them.
+     * @param type
+     * @return
+     */
+    public static List<Bson> getIdexes(String type) {
+
+        List<Bson> indexes = new ArrayList<>();
+
+        switch (type){
+            case "Training":
+                indexes.add(
+                        Indexes.compoundIndex(
+                                Indexes.ascending(Training.CREATION_DATE_FIELD_NAME)
+                        )
+                );
+                break;
+        }
+
+        return indexes;
+    }
+
+}
